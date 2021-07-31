34f09027daa7480dda668eea71258ecf2317ca2f
João Pedro Picolo <picolojoaop@gmail.com>
2021-07-31 19:59:43 -0300

    Adds navigation controller

--- NanoCrashChallenge2/ViewController.swift
+++ NanoCrashChallenge2/ViewController.swift
@@ -13,20 +13,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet var inspirationText: UILabel!
     @IBOutlet var profileName: UILabel!
     @IBOutlet var profilePicture: UIImageView!
-    @IBOutlet var table: UITableView!
-    
-    @IBAction func startQuizz() {
-        let vc = storyboard?.instantiateViewController(identifier: "quizz") as! QuizzViewController
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true)
-    }
+    @IBOutlet var categoriesTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        table.register(CategoryTableViewCell.nib(), forCellReuseIdentifier: CategoryTableViewCell.identifier)
-        table.delegate = self
-        table.dataSource = self
+        categoriesTable.register(CategoryTableViewCell.nib(), forCellReuseIdentifier: CategoryTableViewCell.identifier)
+        categoriesTable.delegate = self
+        categoriesTable.dataSource = self
         setUpCategories()
         
         inspirationText.text = "\"If you can’t explain something in simple terms, you don't understand it.\""
@@ -45,7 +39,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         categories.append(Category(name: "Chemistry", description: "Check out 7 questions about the science of matter", iconName: "chemistry"))
     }
     
-    // Table View Functions
+    // Table functions
     func numberOfSections(in tableView: UITableView) -> Int {
         return categories.count
     }
@@ -64,13 +58,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: CategoryTableViewCell.identifier, for: indexPath) as! CategoryTableViewCell
         cell.configure(category: categories[indexPath.section].name, description: categories[indexPath.section].description, iconName: categories[indexPath.section].iconName)
         
+        let selectedView = UIView()
+        selectedView.backgroundColor = UIColor.systemGray
         
         // Adds border to cell
         cell.contentView.layer.borderWidth = 0.4
         cell.contentView.layer.borderColor = UIColor(named: "projectPurple")!.cgColor
         cell.contentView.layer.cornerRadius = 5
+        cell.selectedBackgroundView = selectedView
 
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        categoriesTable.deselectRow(at: indexPath, animated: true)
+        
+        performSegue(withIdentifier: "startQuizz", sender: self)
+    }
+    
+    // Navigation Controller functions
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Hide navigation bar on this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // Show the navigation bar on next view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
 
